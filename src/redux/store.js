600aeee9c95e29fae6f48fdf5a359b65719ffc42
Javier Duque <javier.duque@mectronics.co>
2022-05-04 18:21:05 -0500

    Update store.js
    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import { persistReducer } from "reduxjs-toolkit-persist";
-import storage from "reduxjs-toolkit-persist/lib/storage";
 
 //Reducers
 import { authReducer } from "./reducers/authReducer";
@@ -19,15 +17,6 @@ const reducers = combineReducers({
   UI: UIReducer,
   admin: adminReducer,
 });
-
-const persistConfig = {
-  key: "root",
-  storage,
-  // blacklist: ['navigation'], // navigation sin persistencia
-  // whitelist: ['navigation'], // Sólo navigation con persistencia
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
