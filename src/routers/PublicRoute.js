576316f7300243d0129ae5b89e567e22b671501a
Javier Duque <javier.duque@mectronics.co>
2022-05-18 11:18:36 -0500

    Update Nginx

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,14 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useIsAuthenticated } from "@azure/msal-react";
 import { Navigate, useNavigate } from "react-router-dom";
 
 export const PublicRoute = ({ children }) => {
   const isAuthenticated = useIsAuthenticated();
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    isAuthenticated && navigate("/admin/");
-  }, [isAuthenticated]);
-
-  return isAuthenticated ? <Navigate to="/admin" /> : children;
+  return isAuthenticated ? <Navigate to="/admin/" /> : children;
 };
